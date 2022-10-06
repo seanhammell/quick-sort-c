1f92755b74e97b191690f3cf733426decf0670d0
Sean Hammell <sean.m.hammell@gmail.com>
2022-10-05 21:22:34 -0500

    Add swap function

--- quick-sort.c
+++ quick-sort.c
@@ -2,6 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define KNRM "\x1b[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+void swap(double* px, double* py) {
+    double temp;
+
+    temp = *px;
+    *px = *py;
+    *py = temp;
+}
+
+void test_swap() {
+    double list[5] = {1, 2, 3, 4, 5};
+    double checklist[5] = {1, 4, 3, 2, 5};
+
+    swap(&list[1], &list[3]);
+
+    for(int i = 0; i < 5; ++i) {
+        if (list[i] != checklist[i]) {
+            printf("%s[FAILED]%s test swap\n", KRED, KNRM);
+            return;
+        } 
+    }
+
+    printf("%s[PASSED]%s test swap\n", KGRN, KNRM);
+}
+
 int main(int argc, char* argv[argc+1]) {
     if (argc > 1) {
         int size = argc - 1;
@@ -12,6 +40,8 @@ int main(int argc, char* argv[argc+1]) {
         }
         printf("\n");
     }
+
+    test_swap();
 
     return EXIT_SUCCESS;
 }
